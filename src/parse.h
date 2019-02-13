3ce4d197a8bd0728b1946ac37d910a4b7cdc7633
hanaywy@gmail.com <hanaywy@gmail.com>
2019-02-12 20:13:25 -0500

    1

--- src/parse.h
+++ src/parse.h
@@ -4,5 +4,29 @@
 
 #ifndef CACHING_PROXY_PARSE_H
 #define CACHING_PROXY_PARSE_H
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class parse_req{
+private:
+	string request;
+	string type;
+	string url;
+	string host;
+	string port;
+
+public:
+	parse_req();
+	parse_req(string request);
+	void parse_tool();
+	string get_type();
+	string get_url();
+	string get_host();
+	string get_port();
+};
 
 #endif //CACHING_PROXY_PARSE_H
